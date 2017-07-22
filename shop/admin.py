b57abf9b27b6903d157887ea69f982d302a0bd74
Yunki <shinki95@gmail.com>
2017-07-22 21:24:52 +0900

    Revert "eatya 프로젝트 생성"
    This reverts commit 1718c61487d26b6735105f6c44892d2f05c9fbea.

--- shop/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Shop
-
-
-class ShopAdmin(admin.ModelAdmin):
-     list_dispaly = ['id', 'menu', 'price', 'updated_at']
-admin.site.register(Shop, ShopAdmin)
--- shop/forms.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-from django import forms
-from .models import Shop
-
-class ShopModelForm(forms.ModelForm):
-    class Meta:
-        model = Shop
-        fields = ['name', 'menu', 'rating','price']
-        
